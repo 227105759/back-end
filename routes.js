3400c831bd6a8e0e65fa6d1c54714b3630d88332
227105759 <122469935+227105759@users.noreply.github.com>
2023-06-19 10:30:03 +0800

    Backend V2
    1. The edit and delete cat list function  has created

--- routes.js
+++ routes.js
@@ -2,77 +2,259 @@ const express = require("express");
 const { db, firebaseStorage } = require("./firebaseAdmin"); // Import db and firebaseStorage
 const router = express.Router();
 const multer = require("multer");
-const {storage} = multer.memoryStorage();
+const { storage } = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 
 //get cat list data
 router.get("/cats", async (req, res) => {
-    try {
-        let catQuery = db.collection("cats");
+  try {
+    let catQuery = db.collection("cats");
 
-        const catSnapshot = await catQuery.get();
-        const catData = [];
-        catSnapshot.forEach((doc) => {
-          catData.push({ id: doc.id, ...doc.data() });
-        });
-        res.status(200).json(catData);
-    } catch (error){
-        res.status(500).json({ error: "Error fetching cat data" });
-    }
+    const catSnapshot = await catQuery.get();
+    const catData = [];
+    catSnapshot.forEach((doc) => {
+      catData.push({ id: doc.id, ...doc.data() });
+    });
+    res.status(200).json(catData);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching cat data" });
+  }
 });
 
+//insert
+router.post("/add-cat", upload.single("images"), async (req, res) => {
+  try {
+    const { title, age } = req.body;
+    const images = req.file;
+
+    const catRef = db.collection("cats").doc();
+    await catRef.set({
+      id: catRef.id,
+      title,
+      age,
+    });
+
+    const bucket = firebaseStorage.bucket();
+    const imageFileName = `cats/${catRef.id}/${images.originalname}`;
+    const imageFile = bucket.file(imageFileName);
+    const stream = imageFile.createWriteStream({
+      metadata: {
+        contentType: images.mimetype,
+      },
+    });
+
+    stream.on("error", (err) => {
+      console.error(err);
+      return res.status(500).send(err);
+    });
+
+    stream.on("finish", async () => {
+      // Get the public URL of the uploaded image
+      const publicUrl = await imageFile.getSignedUrl({
+        action: "read",
+        expires: "03-01-2500",
+      });
+
+      await catRef.update({ images: publicUrl[0] });
+
+      res.status(200).send({ success: true, catId: catRef.id });
+    });
+
+    stream.end(images.buffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
 
 //update
-router.post("/add-cat", upload.single("images"), async(req, res) => {
-    try{
-        const { title, age } = req.body;
-        const images = req.file;
+router.put("/cats/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, age  } = req.body;
 
-        const catRef = db.collection("cats").doc();
-        await catRef.set({
-            id: catRef.id,
-            title,
-            age
-        })
+    const catRef = db.collection("cats").doc(id);
+    await catRef.update({
+        title,
+        age,
+    });
 
-        const bucket = firebaseStorage.bucket();
-        const imageFileName = `cats/${catRef.id}/${images.originalname}`;
-        const imageFile = bucket.file(imageFileName);
-        const stream = imageFile.createWriteStream({
-            metadata: {
-              contentType: images.mimetype,
-            },
-          });
+    res.status(200).json({ message: "Cat entry updated successfully: ${id} "});
+  } catch (error) {
+    console.error("Error updating cat entry:", error);
+    res.status(500).json({ error: "Error updating cat entry: ${error.message}" });
+  }
+});
 
-          stream.on("error", (err) => {
-            console.error(err);
-            return res.status(500).send(err);
-          });
+//delete
+router.delete("/cats/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await db.collection("cats").doc(id).delete();
+    res.status(200).json({ message: "Cat entry deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting cat entry:", error);
+    res.status(500).json({ error: "Error deleting cat entry" });
+  }
+});
 
-          stream.on("finish", async () => {
-            // Get the public URL of the uploaded image
-            const publicUrl = await imageFile.getSignedUrl({
-              action: "read",
-              expires: "03-01-2500",
-            });
+//insert
+router.post("/add-favorite",  async (req, res) => {
+  try {
+    const { id } = req.body;
+    //const images = req.file;
 
-            await catRef.update({ images: publicUrl[0] });
+    const catRef = db.collection("albums").doc();
+    await catRef.set({
+      id: catRef.id,
+    });
 
-            res.status(200).send({ success: true, catId: catRef.id });
-          });
-      
-          stream.end(images.buffer);
+    stream.on("error", (err) => {
+      console.error(err);
+      return res.status(500).send(err);
+    });
 
-    } catch (error){
-        console.error(error);
-        res.status(500).send(error);
-    }
-})
+    stream.on("finish", async () => {
 
+      res.status(200).send({ success: true, catId: catRef.id });
+    });
 
+    stream.end(images.buffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
 
-
-
-module.exports = router
-
+module.exports = router;
+/**
+ * openapi: 3.0.0
+info:
+  version: 1.0.0
+  title: Cat API
+  description: A RESTful API for managing cat data
+servers:
+  - url: http://localhost:3000
+paths:
+  /cats:
+    get:
+      summary: Get a list of cats
+      responses:
+        '200':
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  type: object
+                  properties:
+                    id:
+                      type: string
+                    title:
+                      type: string
+                    age:
+                      type: integer
+                    images:
+                      type: string
+    post:
+      summary: Add a new cat
+      requestBody:
+        content:
+          multipart/form-data:
+            schema:
+              type: object
+              properties:
+                title:
+                  type: string
+                age:
+                  type: integer
+                images:
+                  type: string
+                  format: binary
+      responses:
+        '200':
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  success:
+                    type: boolean
+                  catId:
+                    type: string
+  /cats/{id}:
+    put:
+      summary: Update a cat by ID
+      parameters:
+        - name: id
+          in: path
+          required: true
+          description: ID of the cat to update
+          schema:
+            type: string
+      requestBody:
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                title:
+                  type: string
+                age:
+                  type: integer
+      responses:
+        '200':
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  message:
+                    type: string
+    delete:
+      summary: Delete a cat by ID
+      parameters:
+        - name: id
+          in: path
+          required: true
+          description: ID of the cat to delete
+          schema:
+            type: string
+      responses:
+        '200':
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  message:
+                    type: string
+  /add-favorite:
+    post:
+      summary: Add a cat to favorites
+      requestBody:
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                id:
+                  type: string
+      responses:
+        '200':
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  success:
+                    type: boolean
+                  catId:
+                    type: string
+ * 
+ */
